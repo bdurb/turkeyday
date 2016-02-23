672bdf309d3a4c910d8216076acbcd52e37b034a
Brian Durbin <brian@rpmgreenville.com>
2016-02-23 16:35:57 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turkeyday::Application.config.secret_key_base = '0f3ab9ca201a1d4d2df383c0847c609c3335d9fd124be6f68367b480651d08f6897744a8b271279c3e2a633abafa4d30c11aa551cfe0217fa6ed2b182dcfb5d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turkeyday::Application.config.session_store :cookie_store, key: '_turkeyday_session'
